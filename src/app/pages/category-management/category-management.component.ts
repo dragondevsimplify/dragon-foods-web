ebcb5d3c085b3369da554c660c7fd16982ea36dc
dragondevsimplify <dragondevsimplify@gmail.com>
2024-11-21 10:04:50 +0700

    Create base components & pages

---
+++ src/app/pages/category-management/category-management.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-category-management',
+  standalone: true,
+  imports: [],
+  templateUrl: './category-management.component.html',
+})
+export class CategoryManagementComponent {
+
+}
---
+++ src/app/pages/food-management/food-management.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-food-management',
+  standalone: true,
+  imports: [],
+  templateUrl: './food-management.component.html',
+})
+export class FoodManagementComponent {
+
+}
