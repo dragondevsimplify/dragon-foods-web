9cfb99f3351041d06a83a77726783fee64ae2713
dragondevsimplify <dragondevsimplify@gmail.com>
2024-11-21 16:21:05 +0700

    Add header component

--- src/app/layouts/admin/admin.component.ts
+++ src/app/layouts/admin/admin.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './admin.component.html',
 })
 export class AdminComponent {
