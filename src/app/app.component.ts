472c5bdf92d06e93d7d64ae4c4202eec3c99f568
dragondevsimplify <dragondevsimplify@gmail.com>
2024-11-21 11:09:52 +0700

    Setup auth layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { AdminComponent } from './layouts/admin/admin.component';
+import { AuthComponent } from './layouts/auth/auth.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, AdminComponent, AuthComponent],
   templateUrl: './app.component.html',
 })
-export class AppComponent {
-  title = 'web';
-}
+export class AppComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { AdminComponent } from './layouts/admin/admin.component';
+import { AuthComponent } from './layouts/auth/auth.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/admin/login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'auth',
+    component: AuthComponent,
+    children: [
+      {
+        path: 'login',
+        loadChildren: () => import('./views/auth/login/login.module').then(m => m.LoginModule)
+      },
+      {
+        path: 'register',
+        loadChildren: () => import('./views/auth/register/register.module').then(m => m.RegisterModule)
+      },
+    ]
+  }
+];
